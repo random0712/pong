ae3e12d2b1fe489d3c2fc520ec59eb6c1fe4c3b3
NatanMelo7 <63268289+NatanMelo7@users.noreply.github.com>
2023-05-30 19:21:42 -0300

    Update Menu.java

--- src/main/java/pong/menu/Menu.java
+++ src/main/java/pong/menu/Menu.java
@@ -1,10 +1,12 @@
 package pong.menu;
 
-import com.jogamp.opengl.util.awt.TextRenderer;
-import pong.Renderer;
-import pong.cena.Cena;
+import java.awt.Color;
+import java.awt.Font;
 
-import java.awt.*;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.util.awt.TextRenderer;
+
+import pong.Renderer;
 
 public class Menu {
     private TextRenderer textRenderer;
@@ -21,8 +23,19 @@ public class Menu {
         this.desenhaTexto(15, 560, Color.BLACK, frase, 25);
     }
     
-    public void start() {
-        this.desenhaTexto(140, 300, Color.LIGHT_GRAY, "Aperte Enter para começar", 30);
+    public void start(GL2 gl) {
+    	this.iluminacao(gl);
+    	this.desenhaTexto(25, 550, Color.GREEN, "Regras e Comandos:", 30);
+    	this.desenhaTexto(50, 520, Color.LIGHT_GRAY, "A cada rebatida na bola a pontuação será", 20);
+    	this.desenhaTexto(50, 500, Color.LIGHT_GRAY, "incrementada 25 pontos, até atingir 200 pontos", 20);
+    	this.desenhaTexto(50, 480, Color.LIGHT_GRAY, "Se o jogador atingir 200 pontos a 2ª fase será liberada", 20);
+    	this.desenhaTexto(50, 460, Color.LIGHT_GRAY, "Caso o jogador atinja os 400 pontos, vencerá o jogo!", 20);
+    	this.desenhaTexto(50, 440, Color.LIGHT_GRAY, "No entanto, se o jogador errar 5 rebatidas, o jogo termina!", 20);
+    	this.desenhaTexto(100, 350, Color.GREEN, "ENTER: Start", 20);
+    	this.desenhaTexto(100, 300, Color.GREEN, "P: Pause",20);
+    	this.desenhaTexto(100, 250, Color.GREEN, "ESC: Stop", 20);
+    	this.desenhaTexto(100, 200, Color.GREEN, "🠔➝: Deslocamento do Bastão", 20);
+        this.desenhaTexto(180, 100, Color.LIGHT_GRAY, "Aperte Enter para começar", 20);
     }
 
     public void segundaFase() {
@@ -43,5 +56,26 @@ public class Menu {
 
     public void venceu() {
         this.desenhaTexto(175, 300, Color.GREEN, "Você venceu!", 40);
+    }
+    
+    private void iluminacao(GL2 gl) {
+        // Luz especular
+    	gl.glEnable(GL2.GL_LIGHTING);
+        gl.glEnable(GL2.GL_LIGHT0);
+        gl.glEnable(GL2.GL_COLOR_MATERIAL);
+
+        //  Definindo a posição da fonte de luz:
+        float[] lightPosition = {1, 1, 0.0f, 1.0f};
+        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, lightPosition, 0);
+        
+        float[] ambient = {0.2f, 0.2f, 0.2f, 1.0f};
+        float[] diffuse = {1.0f, 0.0f, 0.0f, 1.0f};
+        float[] specular = {1.0f, 1.0f, 1.0f, 1.0f};
+        float shininess = 10.0f;
+
+        gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_AMBIENT, ambient, 0);
+        gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, diffuse, 0);
+        gl.glMaterialfv(GL2.GL_FRONT_AND_BACK, GL2.GL_SPECULAR, specular, 0);
+        gl.glMaterialf(GL2.GL_FRONT_AND_BACK, GL2.GL_SHININESS, shininess);
     }
 }
